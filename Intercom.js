eacaa3d5ca663c31becfe3190ff6959a732f0f7a
Keith Mancuso <keithmancuso@gmail.com>
2014-12-14 23:52:05 -0500

    New version without the visualforce page
    moved everything to query string variables on the js file

--- Intercom.js
+++ Intercom.js
@@ -1,41 +1,39 @@
-var iframe;
+// get the scripts tags on the page
+var scripts = document.getElementsByTagName('script');
+var thisScript = scripts[ scripts.length - 1 ];
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+// hide the block that has the script tag in it
+thisScript.parentElement.parentElement.parentElement.parentElement.style.display = "none"
 
+var queryString = thisScript.src.replace(/^[^\?]+\??/,'');
+var params = parseQuery( queryString );
 
-
-	iframe = document.querySelector('iframe[title=Intercom]');
-
-	var readyStateCheckInterval = setInterval(function() {
-
-	    if (iframe.contentDocument.readyState === "complete") {
-	        initIntercom();
-	        clearInterval(readyStateCheckInterval);
-	    }
-
-	}, 100);
-
-
-});
-
-
-function initIntercom () {
-	console.log("Iframe is now loaded.", iframe.contentDocument.getElementById('intercom-name'));
-
-
-	window.intercomSettings = {
-	  name: iframe.contentDocument.getElementById('intercom-name').innerHTML,
-	  email: iframe.contentDocument.getElementById('intercom-email').innerHTML,
-	  app_id: "aadhbcv9",
-	  "company": {
-	    "id": iframe.contentDocument.getElementById('intercom-company-id').innerHTML,
-	    "name": iframe.contentDocument.getElementById('intercom-company-name').innerHTML,
-	    "plan": "pro",
-	  }
-	  
-	};
-
+function parseQuery ( query ) {
+	var Params = new Object ();
+	if ( ! query ) return Params; // return empty object
+	var Pairs = query.split(/[;&]/);
+	for ( var i = 0; i < Pairs.length; i++ ) {
+		var KeyVal = Pairs[i].split('=');
+		if ( ! KeyVal || KeyVal.length != 2 ) continue;
+		var key = unescape( KeyVal[0] );
+		var val = unescape( KeyVal[1] );
+		val = val.replace(/\+/g, ' ');
+		Params[key] = val;
+	}
+	return Params;
 }
 
-	(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/aadhbcv9';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()                                 
 
+window.intercomSettings = {
+	name: params['name'],
+	email: params['email'],
+	app_id: params['appId'],
+	"company": {
+		"id": params['orgId'],
+		"name": params['company'],
+		"plan": "pro",
+		"system": "salesforce"
+	}
+};
+
+(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/aadhbcv9';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
--- Readme.md
+++ Readme.md
@@ -2,4 +2,24 @@
 
 A simple way to inject the intercom chat widget into every page in salesforce.  
 
-Designed for Salesforce consultants to provide support to their customers directly within salesforce
+Designed for Salesforce consultants to provide support to their customers directly within salesforce.
+
+Just a few simple steps and you'll be up and running
+
+1. Create a new static resource and upload this js file to it
+2. Create a new "Custom Link" under Customize > Home > Custom Links and name it intercom
+3. Set behavior to "Execute Javascript" and leave the content source to "OnClick Javascript" (but dont worry your users wont have to click anything)
+4. Paste the following code into the content box, and make sure to replace YOURAPPIDHERE with your intercom app Id:
+
+```
+{!REQUIRESCRIPT("/resource/Intercom"+"?email="+$User.Email+"&name="+$User.FirstName+" "+ $User.LastName+"&company="+$Organization.Name+"&orgId="+Organization.Id+"&appId=YOURAPPIDHERE")}
+```
+
+5. Create a new "Custom Component" under Customize > Home > Homepage Components and Name it "Intercom" and select the type "Links"
+6. Under Custom Links to show select the "Intercom" link you made in step 4
+7. Then you have to add the component to any homepage layout you want it to show for, so go to Customize > Home > Home page Layouts and edit all of the layouts currently in use.
+8. Under "Select Narrow Components to Show" select your new "Intercom" Component.
+9. On the next screen, the default component ordering is fine, just save the layout.
+10. And finally if you havent already make sure "Show Custom Sidebar Components on All Pages" is checked, under Customize > User Interface
+
+And you should be all set, every standard page (or custom page with the sidebar) should show the intercom widget!
